18a1939846f0b47435900fc59abc0884c202c004
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-16 20:28:40 +0200

    remove db rake

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '46.101.120.93', user: 'deploy', roles: %w{web app db}
+server '46.101.120.93', user: 'deploy', roles: %w{web app}
 set :branch, 'DeployTestCapistrano'
 
 # server-based syntax
