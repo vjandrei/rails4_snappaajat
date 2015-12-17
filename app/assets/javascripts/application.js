70ec3b19ed4a7e2b63fe72aa7d65c09d0ec5df2b
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-17 08:07:56 +0200

    views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,64 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+//= require maj-text-counter
+//= require jquery.tokeninput
+//= require profiles
+//= require jquery.jcrop
+//= require papercrop
+
+
+// Detect file input support
+var isFileInputSupported = (function () {
+  // Handle devices which falsely report support
+  if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+    return false;
+  }
+  // Create test element
+  var el = document.createElement("input");
+  el.type = "file";
+  return !el.disabled;
+})();
+
+// Add 'fileinput' class to html element if supported
+if (isFileInputSupported) {
+  document.documentElement.className += " fileinput";
+}
+
+$( document ).ready(function() {
+	
+	$('#profile_description').majTextCounter({
+		carLimit: 160,
+		words: "",
+		letters: "kirjainta",
+		separator: "",
+		maximum: " Maksimimäärä : ",
+	});
+	
+	$('#profile_image').on('change', function(event) {
+	    var files = event.target.files;
+	    var image = files[0]
+	    var reader = new FileReader();
+	    reader.onload = function(file) {
+	      var img = new Image();
+	      console.log(file);
+	      img.src = file.target.result;
+	      $('#profile_image_target').html(img);
+	      $('#profile_image_exists').remove();
+	    }
+	    reader.readAsDataURL(image);
+	    console.log(files);
+	});
+    
+    
+     $(".profilesSnapCodeScan").on("click", function () {  
+        $(this).parent().find(".profilesSnapCode").slideToggle(function() {
+	       $(this).addClass('animated bounceInDown').css('top', '0px');
+        });
+        
+    });
+
+});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
+  	if params[:category].blank?
+		@profiles = Profile.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 30)
+	else
+		@category_id = Category.find_by(name: params[:category]).id
+		@profile = Profile.where(category_id: @category_id).order("created_at DESC")
+	end
+  end
+  def tags
+	if params[:tag].present? 
+	  @profiles = Profile.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 12)
+	else
+	  @profiles = Profile.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+	end  
   end
 end
