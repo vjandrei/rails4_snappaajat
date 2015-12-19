910487872735d5ecd658921a440990e077808175
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-19 23:33:35 +0200

    test
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'http://46.101.120.93/' }
+  
+  config.assets.compress = false
+  
+  config.serve_static_assets = false
 
 end
