21e02d356e53cc96b305e494678a4e09f272cc34
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-31 12:47:05 +0200

    forum site and google form
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'tags/:tag' => 'pages#tags', to: 'tags#tags', as: :tag
   get "about" => "pages#about"
-
+  get "forum" => "pages#forum"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
