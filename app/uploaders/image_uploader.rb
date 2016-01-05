569fba9b1dede9df5f0e61bebd976dbf098c8151
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-05 10:32:09 +0200

    image resize
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
-  process resize_to_fit: [100, 100]
+  process resize_to_fit: [500, 500]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
