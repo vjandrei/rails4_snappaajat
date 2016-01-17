545c061379c355e52ac433f3d3865ae052f57d12
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-17 16:43:51 +0200

    config for env
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :deploy_to, '/home/deploy/rails4_snappaajat'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml .rbenv-vars}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, fetch(:linked_dirs) + %w{public/uploads}
 
