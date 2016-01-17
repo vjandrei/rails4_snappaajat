f3a63df7d03b0630610e8db3d5dd406eac5952f0
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-17 09:42:40 +0200

    test email password reset
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,23 +77,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'snappaajat.fi' }
-  
-  Rails.application.routes.default_url_options[:host] = 'snappaajat.fi'
-  
+  Rails.application.routes.default_url_options[:host] = '46.101.244.151'
+  config.action_mailer.default_url_options = { host: '46.101.244.151' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
+  domain: "snappaajat.fi",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"]
   }
+  
 
 end
