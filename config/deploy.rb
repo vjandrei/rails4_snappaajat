66d4b00fa678341e46f93c83c67145b82a3e1b2e
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-19 07:55:51 +0200

    remove bin to test rails c production
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :deploy_to, '/home/deploy/rails4_snappaajat'
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, fetch(:linked_dirs) + %w{public/uploads}
 
 # Default value for default_env is {}
