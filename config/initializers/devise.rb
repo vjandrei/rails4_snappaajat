4089bde5f301007b321c00410778c9970c98c001
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-19 21:38:16 +0200

    test
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Snappaajat.fi'  ENV["email_username"]
+  config.mailer_sender =  ENV["email_username"]
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
