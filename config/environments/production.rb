dac1cabaa522679046dd3eafd4402b13a6003bba
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-19 21:11:50 +0200

    missing do
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  config.after_initialize
+  config.after_initialize do
 	  Rails.application.routes.default_url_options[:host] = 'http://46.101.244.151'
 	  config.action_mailer.default_url_options = { host: 'http://46.101.244.151' }
 	  config.action_mailer.delivery_method = :smtp
