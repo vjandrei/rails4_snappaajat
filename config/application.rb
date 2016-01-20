22b75de2aa11a0db6007b3cbbed825b770740cf4
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-20 13:20:53 +0200

    gem 'dotenv-rails', :require => 'dotenv/rails-now'
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
