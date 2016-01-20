0a9620f4e3ea2afa22bd68f422899831072e3429
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-20 10:39:59 +0200

    remove symlink
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,10 +48,6 @@ namespace :deploy do
       #   execute :rake, 'cache:clear'
       # end
     end
-    desc 'Symlink shared directories and files'
-	 task :symlink_directories_and_files do
-	 run "ln -s #{shared_path}/config/application.yml #{release_path}/config/application.yml"
-    end
   end
 end
 
