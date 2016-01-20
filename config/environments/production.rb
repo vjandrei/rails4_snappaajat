a1463bc64154231c71539cba3581ef882de86910
vjandrei <andreas.koutsoukos@gmail.com>
2016-01-20 19:26:34 +0200

    remove  config.after_initialize
    Signed-off-by: vjandrei <andreas.koutsoukos@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,6 @@ Rails.application.configure do
 	  config.action_mailer.perform_deliveries = true
 	  config.action_mailer.raise_delivery_errors = false
 	  config.action_mailer.default :charset => "utf-8"
-	  config.after_initialize do
 		  config.action_mailer.smtp_settings = {
 			  address: "smtp.gmail.com",
 			  port: 587,
@@ -95,6 +94,5 @@ Rails.application.configure do
 			  user_name: ENV["GMAIL_USERNAME"],
 			  password: ENV["GMAIL_PASSWORD"]
 		  }
-	  end
 
 end
