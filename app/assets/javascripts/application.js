8f616b6dac822a694b5ddd5f922875784279981f
Andreas Koutsoukos <andreas.koutsoukos@eficode.com>
2016-01-31 19:28:10 +0200

    mobile view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,40 +47,6 @@ $(function() {
         });
     });
     
-    (function(window){
-
-    // check for touch
-    if (Modernizr.touch) {
-
-        // run the forEach on each figure element
-        [].slice.call(document.querySelectorAll("profilesImage")).forEach(function(el,i){
-
-            // check if the user moves a finger
-            var fingerMove = false;
-            el.addEventListener("touchmove",function(e){
-                e.stopPropagation();
-                fingerMove = true;
-            });
-
-            // always reset fingerMove to false on touch start
-            el.addEventListener("touchstart",function(e){
-                e.stopPropagation();
-                fingerMove = false;
-            });
-
-            // add hover class if figure touchend and fingerMove is false
-            el.addEventListener("touchend",function(e){
-                e.stopPropagation();
-                if (fingerMove == false) {
-                    classie.toggle(el,"hover");
-                }
-            });
-
-        });
-
-    }
-
-})(window);
 
     
     $( "#profile_location_id" ).select2({
